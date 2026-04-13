080d49f178e983e71731c921b6c70bf3ef5c31a2
Mr. Baguetter <Mr.baguetter1@gmail.com>
2026-04-13 11:14:17 -0500

    Added AutoAddEmpty

---
+++ Assets/Scripts/Editor/AutoAddEmpty.cs
@@ -0,0 +1,43 @@
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using Assets.Scripts.Components;
+
+namespace Assets.Scripts.Editor
+{
+    // Todo: Test
+    [InitializeOnLoad]
+    public class AutoAddEmpty
+    {
+        private static HashSet<int> existingObjects = new();
+
+        static AutoAddEmpty()
+        {
+            GameObject[] allObjects = Object.FindObjectsByType<GameObject>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+            foreach (GameObject go in allObjects)
+            {
+                existingObjects.Add(go.GetInstanceID());
+            }
+
+            EditorApplication.hierarchyChanged += OnHierarchyChanged;
+        }
+
+        private static void OnHierarchyChanged()
+        {
+            GameObject[] allObjects = Object.FindObjectsByType<GameObject>(FindObjectsInactive.Include, FindObjectsSortMode.None);
+
+            foreach (GameObject go in allObjects)
+            {
+                if (!existingObjects.Contains(go.GetInstanceID()))
+                {
+                    existingObjects.Add(go.GetInstanceID());
+                    if (go.GetComponent<ObjectBase>() == null)
+                    {
+                        go.AddComponent<EmptyGameObject>();
+                        Debug.Log($"Automatically added YourScript to {go.name}");
+                    }
+                }
+            }
+        }
+    }
+}
